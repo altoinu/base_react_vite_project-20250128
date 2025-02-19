df52e04b767da8ef5670b367bd425562de65d8b7
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-02-19 13:10:22 -0500

    eslint config update, fetch mock

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -3,3 +3,6 @@
 import "@testing-library/jest-dom";
 // https://github.com/testing-library/jest-dom?tab=readme-ov-file#with-jestglobals
 import "@testing-library/jest-dom/jest-globals";
+import fetchMock from "jest-fetch-mock";
+
+fetchMock.enableMocks();
